8d4f309eba4eefd57b26150cd74fbbbeffa45b1b
Franco <franco_tigres18@hotmail.com>
2024-12-15 16:17:52 -0600

    feat|hashear password

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,23 @@
+import type { Request, Response } from 'express';
 import User from '../models/User';
+import { hashPassword } from '../utils/auth';
 
-export const createAccount = async (req, res) => {
+export const createAccount = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    const error = new Error('El usuario ya existe');
+    res.status(409).json({
+      error: error.message,
+    });
+    return;
+  }
+
   // * Forma 2
   const user = new User(req.body);
+  user.password = await hashPassword(password.toString());
 
   await user.save();
 
